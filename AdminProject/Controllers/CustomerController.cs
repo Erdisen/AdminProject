0406e9583a323de4ab69dbebf86e34e927e91202
Erdisen <erdisen16@gmail.com>
2022-11-29 19:00:11 +0300

    Add project files.

---
+++ AdminProject/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet("customer/detail/{id?}")]
+        public IActionResult Detail(int? id)
+        {
+            return View();
+        }
+
+        [HttpGet("customer/list")]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminProject/Controllers/PersonalController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminProject.Controllers
+{
+    
+    public class PersonalController : Controller
+    {
+        [HttpGet("personal/detail/{id?}")]
+        public IActionResult Detail(int? id)
+        {
+            return View();
+        }
+
+        [HttpGet("personal/list")]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
